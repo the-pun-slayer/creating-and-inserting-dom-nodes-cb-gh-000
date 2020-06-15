be05256799823042d6c3c6cef4e2ad42f146307f
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-15 06:25:48 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
 var element = document.createElement('div');
-undefined
+
 element.innerHTML = 'Hello, DOM!';
 element.style.backgroundColor = '#f9f9f9';
-"#f9f9f9"
+
 document.body.appendChild(element);
-<div style=​"background-color:​ rgb(249, 249, 249)​;​">​Hello, DOM!​</div>​
+
 element.style.textAlign = 'center';
-"center"
+
 var ul = document.createElement('ul');
  
 for (let i = 0; i < 3; i++) {
@@ -18,6 +18,3 @@ for (let i = 0; i < 3; i++) {
 }
  
 element.appendChild(ul);
-<ul>​…​</ul>​
-ul.style.textAlign = 'left';
-"left"
